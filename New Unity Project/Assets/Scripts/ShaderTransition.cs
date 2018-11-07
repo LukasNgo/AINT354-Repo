80b82db4f3af76f3c0fa8d21f623f0bdc352d871
(s) Matthew D Bell <mdbell>
2018-11-07 16:40:12 +0000

    Initial Transparency Transition Function
    Added the ability to fade while having two shaders on the same game object

---
+++ New Unity Project/Assets/Scripts/ShaderTransition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderTransition : MonoBehaviour
+{
+    public Material[] mats;
+
+    [Range(0.0f, 1.0f)]
+    public float transparency = 0f;
+
+	void Start ()
+    {
+        mats = GetComponent<Renderer>().materials;
+    }
+	
+	void Update ()
+    {
+        mats[1].SetFloat("_Transparency", transparency);
+	}
+}
