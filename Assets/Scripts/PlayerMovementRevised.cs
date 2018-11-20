5871bf6257a9f0cb2db6982b5813a678b5db897b
DevLiamW <liam.west@students.plymouth.ac.uk>
2018-11-20 17:26:56 +0000

    Movement & Look rotation
    + Movement with rigidbody
    + Creep, sprint and normal speed
    + Look rotation with mouse

---
+++ Assets/Scripts/PlayerMovementRevised.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementRevised : MonoBehaviour {
+    // reference: https://medium.com/ironequal/unity-character-controller-vs-rigidbody-a1e243591483
+
+    private Rigidbody m_rb;
+
+    [SerializeField]
+    private float m_speed = 50f;
+
+    [SerializeField]
+    private float m_sprintSpeed = 100f;
+
+    [SerializeField]
+    private float m_creepSpeed = 10f;
+
+    private bool m_sprint = false;
+    private bool m_creep = false;
+
+    private float horizontal;
+    private float vertical;
+    
+
+    private void Awake()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        m_sprint = Input.GetKey(KeyCode.LeftShift);
+        m_creep = Input.GetKey(KeyCode.Z);
+
+       
+    }
+
+    private void FixedUpdate()
+    {
+        if (!m_sprint && !m_creep)
+        {
+            MovePlayer(m_speed);
+            Debug.Log("Normal");
+        }
+        else if (m_sprint && !m_creep)
+        {
+            MovePlayer(m_sprintSpeed);
+            Debug.Log("Sprint");
+        }
+        else if (!m_sprint && m_creep)
+        {
+            MovePlayer(m_creepSpeed);
+            Debug.Log("Creep");
+        }
+        
+    }
+
+    public void MovePlayer(float _speed)
+    {
+        m_rb.MovePosition(m_rb.position + (transform.forward * vertical) * _speed * Time.fixedDeltaTime);
+        m_rb.MovePosition(m_rb.position + (transform.right * horizontal) * _speed * Time.fixedDeltaTime);
+    }
+}
+
---
+++ Assets/Scripts/mouseLookRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLookRotation : MonoBehaviour {
+
+    // reference: https://forum.unity.com/threads/looking-with-the-mouse.109250/
+
+    private float m_sensitivityX = 15f;
+    private float m_sensitivityY = 15f;
+
+    private float m_rotationY = 0f;
+    private float m_rotationX = 0f;
+
+    [SerializeField]
+    private Camera m_camera;
+
+    private Rigidbody m_rb;
+
+    private void Start()
+    {
+        m_camera = GameObject.Find("Camera").GetComponent<Camera>();
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        m_rotationX += Input.GetAxis("Mouse X") * m_sensitivityX;
+        m_rotationY += Input.GetAxis("Mouse Y") * m_sensitivityY;
+
+        m_rotationY = Mathf.Clamp(m_rotationY, -60f, 60f);
+
+        m_camera.transform.rotation = Quaternion.Euler(-m_rotationY, m_rotationX, 0f);
+    }
+
+    private void FixedUpdate()
+    {
+        m_rb.transform.rotation = Quaternion.Euler(0f, m_rotationX, 0f);
+    }
+}
