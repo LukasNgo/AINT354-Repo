0bb09c6305dff22deaf495dda91ad2199a95c317
LukasNgo <32539108+LukasNgo@users.noreply.github.com>
2018-11-21 21:17:01 +0000

    Attack Player + Follow
    completed sprint to attack player + the monster can now follow whatever destination we give it for example if we put a function on a door it will run towards that door etc. also added animations for attacking and running. if the monster is very close to the player it will attack and deal damage. I also put a test function on PlayerMovement script to trigger following which can be deleted later once everything is tested.

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -10,7 +10,9 @@ public class MonsterController : MonoBehaviour {
     [SerializeField]
     private float wanderTimer = 10;
     [SerializeField]
-    private float detectionRange = 5;
+    private float detectionRange = 10;
+    [SerializeField]
+    private int damageDeal = 50;
     [SerializeField]
     private Transform _player;
     [SerializeField]
@@ -20,6 +22,10 @@ public class MonsterController : MonoBehaviour {
     private NavMeshAgent _agent;
     private float _timer;
     private RaycastHit _raycastHit;
+    private Transform tempDestination;
+    private bool isTimeOut = false;
+
+    public bool isFollowing = false;
 
     void OnEnable()
     {
@@ -33,14 +39,58 @@ public class MonsterController : MonoBehaviour {
 
         _timer += Time.deltaTime;
 
-        if (_timer >= wanderTimer)
+        //walking speed
+        if (isFollowing == false)
+        {
+            _agent.speed = 1.5f;
+        }
+
+        //running speed
+        if (isFollowing == true)
+        {
+            _agent.speed = 4f;
+        }
+
+        //if monster is not following sound or player then roam randomly
+        if (_timer >= wanderTimer && !isFollowing)
         {
             Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
             _agent.SetDestination(newPos);
             _timer = 0;
         }
+
+        //if monster is following something then reaches the desired destination then stop following and set animation to walking
+        if (isFollowing && Vector3.Distance(transform.position, tempDestination.position) < 2)
+        {
+            isFollowing = false;
+            GetComponent<Animator>().SetTrigger("MonsterWalk");
+        }
+
+        //if monster is very close to player, attack the player and follow until player is out of reach
+        if (Vector3.Distance(transform.position, _player.position) < 2)
+        {
+            
+            _agent.SetDestination(_player.position);
+            if (!isTimeOut)
+            {
+                GetComponent<Animator>().SetTrigger("MonsterAttack");
+                isTimeOut = true;
+                StartCoroutine(AttackPlayer(2));
+            }
+            
+        }
     }
 
+    //function to attack player, deal damage and wait x second between attacks
+    private IEnumerator AttackPlayer(float time)
+    {
+        _player.GetComponent<Player>().TakeDamage(damageDeal);
+        yield return new WaitForSeconds(time);
+        isTimeOut = false;
+        GetComponent<Animator>().SetTrigger("MonsterWalk");
+    }
+
+    //calculate random navmesh destination
     public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
     {
         Vector3 randDirection = Random.insideUnitSphere * dist;
@@ -54,6 +104,7 @@ public class MonsterController : MonoBehaviour {
         return navHit.position;
     }
 
+    //detect range from player to trigger echolocation
     private void detectPlayer()
     {
         if (Vector3.Distance(transform.position, _player.position) < detectionRange)
@@ -70,6 +121,15 @@ public class MonsterController : MonoBehaviour {
         {
             echolocation.isEcholocationActive = false;
         }
+    }
+
+    //Use this from other gameobject to set the destination to it: GameObject.FindGameObjectWithTag("Enemy").GetComponent<MonsterController>().SetNewDestination(GetComponent<Transform>());
+    public void SetNewDestination(Transform newDest)
+    {
+        isFollowing = true;
+        tempDestination = newDest;
+        _agent.SetDestination(tempDestination.position);
+        GetComponent<Animator>().SetTrigger("MonsterRun");
     }
 
 }
