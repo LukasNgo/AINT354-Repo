bcbdb8c1bef4ec213a2f16846c17ea2b4beb01c1
lwest <liam.west@students.plymouth.ac.uk>
2018-12-02 13:41:28 +0000

    Light Reduction, Baked Realtime, Flashlight
    + Flashlight asset
    + Can toggle flashlight on and off with space
    + Flash light is affected by creature
    + Reduction of lights to match point pixel light count (12 in HDRP)
    + Attempt to bake realtime lighting
    / Changed lighting intensity

---
+++ Assets/Scripts/ToggleTorch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleTorch : MonoBehaviour {
+
+    private bool m_torchOn = true;
+    private Light m_light;
+
+    private void Start()
+    {
+        m_light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (m_torchOn && m_light.enabled == true)
+            {
+                m_light.enabled = false;
+                m_torchOn = false;
+            }
+            else if (!m_torchOn && m_light.enabled == false)
+            {
+                m_light.enabled = true;
+                m_torchOn = true;
+            }
+        }
+	}
+}
