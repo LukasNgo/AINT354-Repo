3ed4274370c2e35c14b70282d1c497077f1c9e66
DevLiamW <liam.west@students.plymouth.ac.uk>
2018-12-05 16:51:32 +0000

    Title Screen

---
+++ Assets/Menu/Pulse.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour {
+
+
+    private bool pulseOn = false;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("pulse", 1f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pulseOn)
+        {
+            Shader.SetGlobalFloat("_TransparencyMaterial", (Mathf.Lerp(Shader.GetGlobalFloat("_TransparencyMaterial"), 1, Time.deltaTime * 4)));
+        }
+        else
+        {
+            Shader.SetGlobalFloat("_TransparencyMaterial", (Mathf.Lerp(Shader.GetGlobalFloat("_TransparencyMaterial"), 0, Time.deltaTime * 2)));
+        }
+    }
+
+        private void pulse()
+        {
+            if (pulseOn)
+            {
+                pulseOn = false;
+            }
+            else
+            {
+                pulseOn = true;
+            }
+        }
+    
+}
---
+++ Assets/Menu/pulseTitle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pulseTitle : MonoBehaviour {
+
+    [SerializeField]
+    private Projector _projector;
+    [SerializeField]
+    private int min = 10;
+    [SerializeField]
+    private int max = 180;
+
+    private float t = 0;
+
+    [SerializeField]
+    private int duration = 3;
+
+    private void Start()
+    {
+        Destroy(gameObject, duration);
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.right * Time.deltaTime);
+
+        t += Time.deltaTime / duration;
+        _projector.fieldOfView = Mathf.Lerp(min, max, t);
+    }
+
+}
