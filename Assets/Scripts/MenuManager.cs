ebd9ffebff5c427a86023199f199da31da4271c2
Matt <32261725+Vendettan@users.noreply.github.com>
2018-12-05 16:02:36 +0000

    Added animated menus with placeholder settings
    Need to implement settings functions, controls canvas and exit options

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public Animator m_camera;
+    
+    public void CamPosition2()
+    {
+        m_camera.SetBool("Animate", true);
+    }
+
+    public void CamPosition1()
+    {
+        m_camera.SetBool("Animate", false);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+
+}
