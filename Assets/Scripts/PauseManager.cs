33ad51bd5f89adde130aa8ba57eef91e706b3a58
Matt Bell <32261725+Vendettan@users.noreply.github.com>
2018-12-06 16:55:26 +0000

    Added volume & fullscreen options to pause menu

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class PauseManager : MonoBehaviour
 {
     public GameObject m_pauseCanvas;
+    public AudioMixer m_audioMixer;
+    [SerializeField] private Slider m_volumeSlider;
 
     private void Start()
     {
@@ -52,5 +56,22 @@ public class PauseManager : MonoBehaviour
     {
         Cursor.lockState = (Cursor.lockState == CursorLockMode.None) ? CursorLockMode.Locked : CursorLockMode.None;
         Cursor.visible = (Cursor.visible == false) ? true : false;
+    }
+
+    // Set fullscreen according to toggle value
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    // Set master mixer volume to increments of 5 using options slider
+    public void SetVolume(float volume)
+    {
+        float value = m_volumeSlider.value;
+        float interval = 5.0f;
+        value = Mathf.Round(value / interval) * interval;
+
+        m_volumeSlider.value = value;
+        m_audioMixer.SetFloat("volume", value);
     }
 }
